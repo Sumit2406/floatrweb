f22f6f504788ac82d7089b00e4fd3486510c8466
Sumit Kumar <j4sumit@gmail.com>
2023-07-13 18:55:55 +0530

    Description updated for FileUploader, ButtonDemo, RadioButtons, CheckboxDemo and Inputdemo

--- src/screens/Description.js
+++ src/screens/Description.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import FileUploader from '../components/FileUploader'
+import ButtonDemo from '../components/ButtonDemo'
+import RadioButtons from '../components/RadioButtons'
+import CheckboxDemo from '../components/CheckboxDemo'
+import Inputdemo from '../components/Inputdemo'
 
 export default function Description() {
   return (
     <div>
       <h1> Component Discription </h1>
       <div>
-      <h3>Input Box</h3>
+      <h3>Inputdemo Component</h3>
       <p>This compone having icon on left and right</p>
       <p>This component having prefix and suffix like (₹, %)</p>
       <p>This component having Title and placeholder</p>
       <p>we have to pass useState variable textInput and fucntion handleInputChange() as props</p>
+      <p>Props:- </p>
       <p>title-required</p>
       <p>placeholder-required</p>
       <p>prefix/suffix-Optional</p>
@@ -19,34 +25,55 @@ export default function Description() {
       <p>textInput-required</p>
      <p>Link to import:- import Inputdemo from "../components/Inputdemo"</p> 
      <p>Demo:-</p>
-     
       </div>
-
 <div>
-  <h3>Checkbox</h3>
+  <h3>CheckboxDemo Component</h3>
   <p>Created Custom Check box with the lelp of icons</p>
   <p>we need to import Checkbox icons for toggling</p>
   <p>We have used useState variable isChecked for checkbox status</p>
   <p>We have used fucntion handleOnDivChange() for image toggling functionality</p>
+  <p>Props Used:-</p>
+  <p>handleOnDivChange - required</p>
+  <p>isChecked - required</p>
   <p>Link to import:- import CheckboxDemo from '../components/CheckboxDemo' </p> 
 </div>
 
 <div>
-<h3>Radio Button</h3>
+<h3>RadioButtons Component</h3>
   <p>We have used Custom Radion button with the help of icons</p>
   <p>We have imported two radion icon for toggling</p>
   <p>We need to pass useState variable checkd as proos for radio status</p>
   <p>We need to pass fucntion handleradio() as props for image toggling functionality</p>
-  <p>we need to pass arry of object "radioDatas" as props to for updating option values </p>  
+  <p>we need to pass arry of object "radioDatas" as props to for updating option values </p>
+  <p>Props Used:-</p>
+  <p>radioDatas - required</p>  
+  <p>handleradio - required</p>
+  <p>checkd - required</p>
   <p>Link to import:- import RadioButtons from '../components/RadioButtons'</p>
 </div>
 <div>
-  <h3>Button</h3>
+
+  <h3>ButtonDemo Component</h3>
   <p> In button componet we have used props "txt" as button title, "buttonStyle" as style and handleClick as as function and passed all these as props </p>
+  <p>Props Used:- </p>
   <p>txt- required</p>
   <p>buttonStyle - required</p>
   <p>handleClick - handleClick</p>
   <p>Link to import - import ButtonDemo from '../components/ButtonDemo' </p>
+</div>
+
+<div>
+<h3>FileUploader Component</h3>
+<p>We have created custom file upload button with the help of Icon, and fileHandler funtion </p>
+<p>we need to pass fileHandler() fucntion as props </p>
+<p>In this component we have used use state to check the status of uploaded image also used use ref to perform</p>
+<p>In this componet we have used useref to update Dom directly </p>
+<p>Link to import - import FileUploader from '../components/FileUploader'</p>
+<p>Props Used :-</p>
+<p>fileHandler - required </p>
+</div>
+<div>
+  <p>DateComponents component</p>
 </div>
 
     </div>
