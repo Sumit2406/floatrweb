97cd0611606efb7fa7796c75f986a7367f869c58
Sumit Kumar <j4sumit@gmail.com>
2023-07-17 12:44:42 +0530

    changes done in dropdown CSS

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -5,7 +5,9 @@ export default function TextArea({ label, value, onChange }) {
   console.log(value);
   return (
 <div className='TextAreaContainer'>
-      <label className='TextAreaLabel'>{label}</label>
+      {/* <label className='TextAreaLabel'>{label}</label> */}
+      <h2 className='TextAreaLabel'>{label}</h2>
+
       <textarea
         value={value}
         onChange={onChange}
