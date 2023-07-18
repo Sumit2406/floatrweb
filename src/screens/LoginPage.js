d323589af9e1518c17147d83cb53a817e6cba7df
Sumit Kumar <j4sumit@gmail.com>
2023-07-18 15:16:39 +0530

    bottom div added

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -2,6 +2,13 @@ import React, {useState} from 'react'
 import Button from '../components/Button'
 import InputBox from "../components/InputBox"
 import imageWithLaptop from '../assets/pngs/imageWithLaptop.png';
+import group1 from '../assets/pngs/group1.png';
+import group2 from '../assets/pngs/group2.png';
+import group3 from '../assets/pngs/group3.png';
+
+
+
+
 
 export default function LoginPage() {
 //button
@@ -16,6 +23,7 @@ export default function LoginPage() {
       };
 
   return (
+    <div className='login-main-container'>
     <div className='login-wrapper contentblock' >
       <div className="login-left-content"> 
 <h1>Welcome !</h1>
@@ -28,12 +36,17 @@ export default function LoginPage() {
  handleInputChange={handleInputChange}
  value={textInput || ''}
                 />
-
 <Button btnLabel="Login" rectangualar="false" btnClick={btnHandleClick}/>
       </div>
       <div className="login-right-content">
-      <img src={imageWithLaptop} alt="Logo Dark 2" />
+      <img src={imageWithLaptop} alt="imageWithLaptop" />
       </div>
     </div>
+          <div className='login-bottom-content'>
+        <div className='bottom-icons'><img src={group1} alt="group1" /><span> Budget & Expense</span></div>
+        <div className='bottom-icons'><img src={group2} alt="group2" /><span>Financial Goals</span> </div>
+        <div className='bottom-icons'><img src={group3} alt="group3" /><span>Investments </span></div>
+          </div>
+          </div>
   )
 }
