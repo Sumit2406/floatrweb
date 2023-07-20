4dc91a7a77e257eaf88fb0b1137f4042d4b9bb15
Sumit Kumar <j4sumit@gmail.com>
2023-07-20 19:08:40 +0530

    CSS Changes done

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,22 +8,22 @@ export default function Button({btnLabel,rectangualar,btnClick,disable}) {
       backgroundColor: disable ? '#43A2FF' : '#6E7985',
       color: disable ? 'rgba(255, 255, 255, 0.60)': '#FFFFFF',
       fontSize: '1em',
-      width: '200px',
-      letterSpacing:'3px',
-      height: '60px',
-      lineHeight: '22px',
-      borderRadius: '10px',      
+      width: '11.11111111111111em',
+      letterSpacing:'0.16666666666666666em',
+      height: '3.3333333333333335em',
+      lineHeight: '1.2222222222222223em',
+      borderRadius: '0.5555555555555556em',      
       
     }) 
  : ({
   backgroundColor: disable ? '#43A2FF' :'#6E7985',
   color : disable? 'rgba(255, 255, 255, 0.60)' : '#FFFFFF',
   fontSize: '1em',
-  width: '200px',
-  letterSpacing:'3px',
-  height: '60px',
-  lineHeight: '22px',
-  borderRadius: '10px',
+  width: '11.11111111111111em',
+  letterSpacing:'0.16666666666666666em',
+  height: '3.3333333333333335em',
+  lineHeight: '1.2222222222222223em',
+  borderRadius: '0.5555555555555556em',
   
 
 })}
