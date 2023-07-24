8d967cb9fb4dd3f421e0d0f1af6f3de17d4ef376
Sumit Kumar <j4sumit@gmail.com>
2023-07-24 13:17:27 +0530

    Button color has been changed

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,25 +6,25 @@ export default function Button({btnLabel,rectangualar,btnClick,disable}) {
       <button className='btn' 
     style={rectangualar==="true" ? ({
       backgroundColor: disable ? '#43A2FF' : '#6E7985',
-      color: disable ? 'rgba(255, 255, 255, 0.60)': '#FFFFFF',
+      color: disable ? 'rgba(255, 255, 255, 0.60)': '#FFF',
       borderRadius: '0.5rem',      
       width: "100%",
       // height: '70px',
       letterSpacing:'3px',
 lineHeight: '1.5',
 fontSize: '18px',
-fontFamily: 'ProximaBold'
+fontFamily: 'ProximaSemiBold'
     }) 
  : ({
   backgroundColor: disable ? '#43A2FF' :'#6E7985',
-  color : disable? 'rgba(255, 255, 255, 0.60)' : '#FFFFFF',
+  color : disable? 'rgba(255, 255, 255, 0.60)' : '#FFF',
   borderRadius: '0.5rem',      
       width: "100%",
       // height: '70px',
       letterSpacing:'3px',
 lineHeight: '1.5',
 fontSize: '18px',
-fontFamily: 'ProximaBold'
+fontFamily: 'ProximaSemiBold'
 
 })}
       onClick={btnClick}
