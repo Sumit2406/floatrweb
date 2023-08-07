36c52b5e246b9bb53fec5965187a95860728e6ed
Sumit Kumar <j4sumit@gmail.com>
2023-08-07 18:18:46 +0530

    Error paged done

--- src/screens/Errorpage.js
+++ src/screens/Errorpage.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
-
+import Error from "../assets/pngs/Error.png";
+import "../scss/App.scss";
 export default function Errorpage() {
-
-    const navigate=useNavigate();
-    const navigateMainpage=()=>{
-        navigate('/');
-    }
+  const navigate = useNavigate();
+  const navigateMainpage = () => {
+    navigate("/");
+  };
   return (
-    <div className='row d-flex justify-content-center mx-auto'>
-    <div className='col-12 d-flex align-items-center justify-content-center flex-column erropage'>
-      <h1>Oops!</h1>
-      <h2>404 - PAGE NOT FOUND</h2>
-      <p>The page you are looking for might have been removed</p>
-      <p>had its name changed or is temporarily unavailable</p>
-      <Button btnLabel="GO TO HOMEPAGE" rectangualar="false" btnClick={navigateMainpage} disable="true" />
+    <div className="loginBlock container errorpagecontainer">
+      {/* <div className="errorpagecontainer"> */}
+      <p className="ErrorPageTitle">ERROR</p>
+      <div className="errorpage">
+        <img src={Error} alt="Error Page" />
+      </div>
+      <p className="errorpagemsg">Oops! Looks like something rong.</p>
+      <p className="errorpagemsg">We are working on it. Sorry!</p>
+      <div className="errorpagebutton"><Button
+        btnLabel="GO TO HOMEPAGE"
+        rectangualar="false"
+        btnClick={navigateMainpage}
+        disable="true"
+      />
+      </div>
+{/* </div> */}
     </div>
-    
-    </div>
-  )
+  );
 }
